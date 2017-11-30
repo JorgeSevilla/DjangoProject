8deea18a37ac9a4c3d45ad840988268b144e1599
JorgeSevilla <solopunks803@gmail.com>
2017-11-29 22:59:23 -0200

    Modified function index views.py

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(resquest):
-    return HttpResponse("Hello Jorge Sev this is a test")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in lastest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
