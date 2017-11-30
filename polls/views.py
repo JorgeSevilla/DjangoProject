31e6c5eee5373ff78c6bbb95df0b138e158a729b
JorgeSevilla <solopunks803@gmail.com>
2017-11-29 23:49:20 -0200

    Create a render for call object httpresponse

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Question
 
 def index(resquest):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
-    return HttpResponse(output)
+    context = {'lastest_question_list': lastest_question_list,}
+    return render(resquest, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question % s" % question_id)
+    return render("You're looking at question % s" % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    return render(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render("You're voting on question %s." % question_id)
