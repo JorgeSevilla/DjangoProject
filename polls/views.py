ae5ef4fbe0d468a17dcd4e094245dd7bb816e77e
JorgeSevilla <solopunks803@gmail.com>
2017-11-30 00:02:03 -0200

    Make exception 404 replace try and exception

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404,
 from .models import Question
 
 def index(resquest):
@@ -7,12 +7,8 @@ def index(resquest):
     context = {'lastest_question_list': lastest_question_list,}
     return render(resquest, 'polls/index.html', context)
 
-
 def detail(request, question_id):
-    try:
-        question = Question.objecs.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/index.html', {'question': question})
 
 def results(request, question_id):
