bdd6cbc362ae26a30e34bc684d423098859ca7fc
JorgeSevilla <solopunks803@gmail.com>
2017-11-29 23:56:21 -0200

    Create a exception

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 def index(resquest):
@@ -8,7 +9,11 @@ def index(resquest):
 
 
 def detail(request, question_id):
-    return render("You're looking at question % s" % question_id)
+    try:
+        question = Question.objecs.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/index.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
